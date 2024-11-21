4fd3aa17642a83510fd3892c6da942d73e5b1938
maticandella <matiascandella94@gmail.com>
2024-11-21 00:23:39 -0300

    ajustes en genre

--- models/genres/GenreModel.js
+++ models/genres/GenreModel.js
@@ -4,7 +4,14 @@ import BaseModel from '../BaseModel.js';
 
 const GenreModel = sequelize.define('Genre', {
     ...BaseModel.getAttributes(), // Herencia de BaseModel
-    
+    overview: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    icon: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
 }, {
     tableName: 'Genres',
     timestamps: false,
